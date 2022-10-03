13706da13833ea0a451869eb3089360d7a17cdd7
Tommy <tommychen@applyeasypro.com>
2022-10-03 11:16:49 +0800

    make it better

--- connections/mongodb.js
+++ connections/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const console = require('../service/console');
 
-const DB = process.env.DATABASE_URL.replace('<password>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE_COMPASS.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB).then(() => {
   console.log('資料庫連線成功');
