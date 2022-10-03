d04db0ac15ae19a38fd09318fddac42e11e256db
Tommy <tommychen@applyeasypro.com>
2022-10-03 11:54:36 +0800

    change node

--- connections/mongodb.js
+++ connections/mongodb.js
@@ -4,7 +4,7 @@ const console = require('../service/console');
 const DB = process.env.DATABASE_COMPASS.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB).then(() => {
-  console.log('資料庫連線成功');
+  console.log('資料庫連線成功1');
 }).catch((error) => {
   console.log(error);
 });
